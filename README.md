750b6148d30c75bc3d44d29a09d40432ac68b179
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2025-03-06 23:55:48 +0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# FilmLane
+# FilmLanE
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
-import dotenv from "dotenv";
-import app from "./app.js"
-dotenv.config();
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
 
+const server = new ApolloServer({
 
-const port = process.env.PORT || 8080;
+})
 
+const { url } = await startStandaloneServer(server, {
+  listen: {
+    port: 3000
+  }
+})
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+console.log("server started at", 3000)
