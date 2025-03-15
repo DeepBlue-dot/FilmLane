d6350eb873b2582f2261af3a4776432c0bc7d0a1
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2025-03-15 23:11:08 +0300

    user controller

--- server/src/controllers/userControllers.ts
+++ server/src/controllers/userControllers.ts
@@ -27,3 +27,59 @@ export const getUser = asyncHandler(
         })
     }
 )
+
+export const getUserById = asyncHandler (
+    async(req, res, next) =>{
+        const user = await prisma.user.findUnique(
+            {
+                where: {
+                    id: req.params.id
+                },
+                select: {
+                    id: true,
+                    email: true,
+                    username: true,
+                    createdAt: true,
+                    updatedAt: true,
+                    passwordHash: false
+                }
+            }
+        )
+        res.status(200).json({
+            status: "success",
+            data: {
+                user: user
+            }
+        })
+    }
+)
+
+export const getAllUsers= asyncHandler(
+    async (req, res, next) => {
+        
+    }
+)
+
+export const updateUser = asyncHandler(
+    async (req, res, next) => {
+        
+    }
+)
+
+export const updateUserById = asyncHandler(
+    async (req, res, next) => {
+        
+    }
+)
+
+export const deleteUser = asyncHandler(
+    async (req, res, next) => {
+        
+    }
+)
+
+export const deleteUserById = asyncHandler(
+    async (req, res, next) => {
+        
+    }
+)
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { requireAuthentication } from "../middleware/authenticateUser.js";
-import { getUser } from "../controllers/userControllers.js";
+import { deleteUser, deleteUserById, getAllUsers, getUser, getUserById, updateUser, updateUserById } from "../controllers/userControllers.js";
 
 const userRoutes: Router = Router()
 
 userRoutes.get("/me", requireAuthentication, getUser)
-userRoutes.get("/:id")
-userRoutes.get("/")
-userRoutes.put("/:id")
-userRoutes.delete("/:id")
+userRoutes.put("/me", requireAuthentication, updateUser)
+userRoutes.delete("/me", requireAuthentication, deleteUser)
+userRoutes.get("/:id", getUserById)
+userRoutes.get("/", getAllUsers)
+userRoutes.put("/:id", updateUserById)
+userRoutes.delete("/:id", deleteUserById)
+
 export default userRoutes
