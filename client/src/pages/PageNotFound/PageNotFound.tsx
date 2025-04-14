dad57600d9f6292986e2a7e1471cd4c2e23e12c9
DeepBlue <126600616+DeepBlue-dot@users.noreply.github.com>
2025-04-14 04:48:46 +0300

    PageNotFound

--- client/src/pages/PageNotFound/PageNotFound.tsx
+++ client/src/pages/PageNotFound/PageNotFound.tsx
@@ -1,3 +1,31 @@
+import { Link } from "react-router-dom";
+
 export default function NotFound() {
-  return <></>;
+  return (
+    <div className="relative min-h-screen w-full bg-slate-950">
+      <div className="absolute bottom-0 left-0 right-0 top-0   bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
+      <main className="relative flex min-h-screen items-center justify-center px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance sm:text-7xl text-white ">
+            Page not found
+          </h1>
+          <p className="mt-6 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
+            Sorry, we couldn’t find the page you’re looking for.
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <Link
+              to={"/home"}
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Go back home
+            </Link>
+            <a href="#" className="text-sm font-semibold text-white">
+              Contact support <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
